e81d6d87f459dfb4419a633e3fe8bf42e21c34b8
madhurajoshi18 <madhura.joshi@stacklab.in>
2023-11-02 09:33:01 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:real_estate_app/controllers/page_view_controller.dart';
 import 'package:real_estate_app/splash_screen.dart';
 
 void main() {
   Get.put(PageViewController());
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
   runApp(MyApp());
 }
 
