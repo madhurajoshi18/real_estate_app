259badcea0c026c53b70c98e1d9616de3a1faf62
madhurajoshi18 <madhura.joshi@stacklab.in>
2023-12-05 07:54:09 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {     
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
